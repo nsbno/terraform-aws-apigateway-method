1fe537346df6ddc55f6342a0168d46c271c1294d
arnhaa <arnehaagensen@gmail.com>
2020-05-12 15:30:49 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -57,9 +57,3 @@ variable "request_parameters" {
   default = {}
 }
 
-# depends_on workaround
-
-variable "depends_on" {
-  description = "Helper variable to simulate depends_on for terraform modules"
-  default     = []
-}
